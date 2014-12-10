9fe3c4c8ffc1843d75f6ecd742c182ea34a7a6f7
Nikolay Yaremko <makiwara@humanemagica.com>
2014-12-10 17:49:29 +0100

    Surface background and 16x128 grid

--- ensography/urls.py
+++ ensography/urls.py
@@ -13,31 +13,8 @@ def static(request, url):
 
 urlpatterns = patterns('',
     #(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.HOST["STATIC_ROOT"]}),
-    (r'^(temp)', 'urls.static'),
-    (r'^stat/([^/]*)/report/([^/]*)', 'booklets.views.stat_report'),
-    (r'^stat/([^/]*)/booklet/([^/]*)', 'booklets.views.stat_booklet'),
-    (r'^stat/([^/]*)/positive/([^/]*)/([^/]*)', 'booklets.views.stat_positive'),
-    (r'^stat/([^/]*)/negative/([^/]*)/([^/]*)', 'booklets.views.stat_negative'),
-    (r'^stat/([^/]*)/page/([^/]*)/([^/]*)', 'booklets.views.stat_page'),
-
-    (r'^test/([^/]*)', 'booklets.views.test'),
-
-    (r'^zen/([^/]*)', 'booklets.views.show'),
-    (r'^one/([^/]*)', 'booklets.views.one'),
-
-
-    (r'^enso/([^/]*)', 'booklets.views.show2'),
-    (r'^two/([^/]*)',  'booklets.views.one2'),
-
-    (r'^setup1', 'booklets.views.setup1'),
-    (r'^setup2', 'booklets.views.setup2'),
-    (r'^setup3', 'booklets.views.setup3'),
-    (r'^setup_students2', 'booklets.views.setup1_students2'),
-    (r'^setup_students', 'booklets.views.setup1_students'),
-
-    url(r'^admin/', include(admin.site.urls)),
-
-    (r'^', 'booklets.views.homepage'),    
+    (r'^(temp)',       'urls.static'),
+    (r'^(article/.*)', 'urls.static'),
 )
 
 
