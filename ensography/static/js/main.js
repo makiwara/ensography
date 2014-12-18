681503cc0fb7d27811ec84aa0ef2e5344ef7c59a
Nikolay Yaremko <makiwara@humanemagica.com>
2014-12-18 23:17:25 +0500

    First take on chrome: icons and TOC
    CSS transitions not provided yet; CSS capitalize also is not provided
    yet.

---
+++ ensography/static/js/main.js
@@ -0,0 +1,46 @@
+$(function(){
+
+
+	$('.control-expandable .icon')
+		.click(function(){
+			show_tooltips();
+			var $ctrl = $(this).parents('.control-expandable');
+			var isExpanded = $ctrl.hasClass('control-expanded');
+			$ctrl.toggleClass('control-expanded');
+			if (isExpanded) $ctrl.find('.control-expansion').slideUp();
+			else			$ctrl.find('.control-expansion').slideDown();
+		})
+		.mouseover(show_tooltips)
+		.mousemove(show_tooltips)
+
+
+	var timeoutTooltips;
+	function hide_tooltips() {
+		$('.icon .icon-text').css({ opacity: 0 })
+	}
+	function show_tooltips() {
+		$('.icon .icon-text').css({ opacity: 1 })
+		clearTimeout(timeoutTooltips)
+		timeoutTooltips = setTimeout(hide_tooltips, 2500);
+	}
+
+	var timeoutTOC;
+	$('.chrome-toc-hint').appendTo($('body'))
+	$('.control-toc .chrome-toc-chapter')
+		.mouseover(function(){
+			clearTimeout(timeoutTOC);
+			$('.chrome-toc-hint').css({
+				opacity: 1,
+				top: $(this).offset().top
+			})
+		})
+		.mouseout(function(){
+			clearTimeout(timeoutTOC);
+			timeoutTOC = setTimeout(function(){ $('.chrome-toc-hint').css('opacity', 0) }, 250)
+		})
+
+
+	show_tooltips();
+	$(window).scroll(show_tooltips);
+
+})
