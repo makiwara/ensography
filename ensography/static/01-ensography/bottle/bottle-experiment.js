847dea837849d5a7410f04684106036032176362
Nikolay Yaremko <makiwara@humanemagica.com>
2015-02-04 07:09:01 +0300

    Intermediate state of experiment implementation

--- ensography/static/01-ensography/bottle/bottle-experiment.js
+++ ensography/static/01-ensography/bottle/bottle-experiment.js
@@ -1,11 +1,10 @@
-window.BottleExperiment = function(container, tag, title) {
-    this.tag = tag;
-    this.title = title;
+window.BottleExperiment = function(container, user) {
     this.$ = $(container);
     window.BottleExperiment.prototype.user = { success: false }; // name, id, email
+    if (user) window.BottleExperiment.prototype.onAuth(user);
     this.view_render();
 }
-window.Comments.prototype = {
+window.BottleExperiment.prototype = {
 
     // MODEL =======================================================================
     // MODEL =======================================================================
@@ -20,62 +19,50 @@ window.Comments.prototype = {
     // CONTROLLERS ================================================================
     onAuth: function(user) {
         window.BottleExperiment.prototype.user = user;
-        if (window.BottleExperiment.prototype.user.success) 
-            this.view_authSuccess();
-        else
-            this.view_authFailure();
-    },
-
-
-    // VIEWS ======================================================================
-    // VIEWS ======================================================================
-    // VIEWS ======================================================================
-    // VIEWS ======================================================================
-    // VIEWS ======================================================================
-    view_suspend: function() {
-        // TODO refactor
-        $('.comments textarea').attr('disabled', true);
-        $('.comments').addClass('m-comments-suspend');
-    },
-    view_activate: function() {
-        // TODO refactor
-        $('.comments textarea').attr('disabled', false);
-        $('.comments').removeClass('m-comments-suspend');
-    },
-    view_authFailure: function() {
-        // TODO refactor
-        // works for all Comments on page
-        $('.m-comments-send-facebook-auth')
-            .removeClass('m-comments-send-facebook-auth')
-            .addClass('m-comments-send-facebook');
-    },
-    view_authSuccess: function() {
-        // TODO refactor
-        // works for all Comments on page
-        window.Comments.prototype.view_activate();
-        $('.m-comments-send-facebook')
-            .addClass('m-comments-send-facebook-auth')
-            .removeClass('m-comments-send-facebook');
-        // patch and show userpics
-        $('.comments-area .userpic').show().css({
-            'background-image' : 'url('+window.Comments.prototype.user.userpic+')'
-        })
-        // focus on single textarea 
-        if (window.Comments.prototype.authSource) {
-            var that = window.Comments.prototype.authSource;
-            var isFocus = false;
-            that.$.find('.comments-host-middle .comments-sheet textarea')
-                .each(function(){ isFocus = true; $(this).focus(); });
-            if (!isFocus) that.$.find('.comments-host-middle .comments-reply-sheet textarea')
-                .each(function(){ $(this).focus(); });
-            window.Comments.prototype.authSource = false;
+        if (this._rendered) {
+            if (window.BottleExperiment.prototype.user.success) 
+                this.view_authSuccess();
+            else
+                this.view_authFailure();
         }
     },
+    onNext: function() {
+        // TODO load next image 
+        console.log('go')
+    },
+
+
+    // VIEWS ======================================================================
+    // VIEWS ======================================================================
+    // VIEWS ======================================================================
+    // VIEWS ======================================================================
+    // VIEWS ======================================================================
+    tick: 250, // animation tick = 0.25s
+    view_authFailure: function() {
+        $('.experiment-auth').show().css({opacity:1})
+        $('.experiment-auth-complete').hide().css({opacity:0})
+    },
+    view_authSuccess: function() {
+        var that = this;
+        $('.experiment-auth').css({opacity:0})
+        setTimeout(function(){
+            $('.experiment-auth').hide();
+            $('.experiment-auth-complete').show().css({opacity:1})
+        }, this.tick);
+        setTimeout(function(){ that.onNext(); }, this.tick*2);
+    },
+    _rendered: false,
     view_render: function() {
-        // TODO refactor       
-        var prerender = [].join("");
-        this.$.html(prerender);
-        this.view_bind();
+        this._rendered = true;
+        var that = this;
+        if (this.user.success) 
+            this.view_authSuccess();
+        else 
+            this.view_authFailure();
+        this.$.find('.js-auth').click(function(e){
+            that.view_auth();
+            return true;
+        })
     },
     view_auth: function() {
         window.BottleExperiment.prototype.authSource = this;
@@ -84,10 +71,6 @@ window.Comments.prototype = {
         var left = (screen.width/2)-(w/2);
         var top = (screen.height/2)-(h/2);
         window.open(window.Comments.loginUrl, 'auth', 'width='+w+',height='+h+',left='+left+',top='+top);
-    },
-    view_bind: function() {
-        // TODO refactor
-        var that = this;
     },
 
 
